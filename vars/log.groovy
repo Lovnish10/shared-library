2a6d51251c1e1e58ef12013998bbe129ebc12b21
Lovnish10 <42857202+Lovnish10@users.noreply.github.com>
2021-11-02 10:42:09 +0530

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -2,7 +2,7 @@ def call(script){
     node(){
     echo 'hello world'
       echo "this value is given by :${script}"
-       println script.getClass().getName()
+       println script.getClass().getSimpleName()
         
     }
 }

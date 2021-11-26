4d41d05548c4d2840ffbc75653ebc4a6787dd4c1
Lovnish10 <42857202+Lovnish10@users.noreply.github.com>
2021-11-26 19:42:54 +0530

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -4,8 +4,8 @@ def call(script){
       echo "this value is given by :${script}"
        println script.getClass().getSimpleName()
         println script
-        println this.BRANCH_NAME
-        println script.BRANCH_NAME
+       // println this.BRANCH_NAME
+       // println script.BRANCH_NAME
         println BRANCH_NAME
         
     }

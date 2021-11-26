fef0e8cf9aafd1bdaddb1cae822095203fe07882
Lovnish10 <42857202+Lovnish10@users.noreply.github.com>
2021-11-26 20:24:40 +0530

    Update AddSidebarLinkAction.groovy

--- src/AddSidebarLinkAction.groovy
+++ src/AddSidebarLinkAction.groovy
@@ -1,32 +1,36 @@
 public class AddSidebarLinkAction implements hudson.model.Action,java.io.Serializable {
-  private String displayName;
-  private String iconFileName;
-  private String urlName;
-  private String iconClassName;
+//   private String displayName;
+//   private String iconFileName;
+//   private String urlName;
+//   private String iconClassName;
+   def script;
   
-  public AddSidebarLinkAction(String displayName,String iconFileName,String urlName,String iconClassName) {
-    this.displayName = displayName;
-    this.iconFileName = iconFileName;
-    this.urlName = urlName;
-    this.iconClassName = iconClassName;
+  public AddSidebarLinkAction(script) {
+//     this.displayName = displayName;
+//     this.iconFileName = iconFileName;
+//     this.urlName = urlName;
+//     this.iconClassName = iconClassName;
+    this.script = script
+    
   }
   
-  @Override 
-  public String getDisplayName() {
-    return displayName;
-  }
+//   @Override 
+//   public String getDisplayName() {
+//     return displayName;
+//   }
   
-  @Override 
-  public String getIconFileName() {
-    return iconFileName;
-  }
+//   @Override 
+//   public String getIconFileName() {
+//     return iconFileName;
+//   }
   
-  @Override 
-  public String getUrlName() {
-    return urlName;
-  }
+//   @Override 
+//   public String getUrlName() {
+//     return urlName;
+//   }
   
-  public String getIconClassName() {
-    return iconClassName;
-  }
+//   public String getIconClassName() {
+//     return iconClassName;
+//   }
+
 }

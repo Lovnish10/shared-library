bb22fd7e1f878cac3e5cd7c3f09ed920a84637c9
Lovnish10 <42857202+Lovnish10@users.noreply.github.com>
2021-12-06 13:09:58 +0530

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,17 +1,12 @@
 import Utilities
 def call(script){
-   Utilities utl = new Utilities(script)
+  // Utilities utl = new Utilities(script)
     
     node(){
-      def my =  utl.maven()
-       println my
+   
     echo 'hello world'
       echo "this value is given by :${script}"
-       println script.getClass().getSimpleName()
-        println script
-       // println this.BRANCH_NAME
-        println script.env.JENKINS_URL
-        println env.JENKINS_URL
+
         
     }
 }

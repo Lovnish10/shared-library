b04f5421520fc6e4aae17459150548c9d52799b2
Lovnish10 <42857202+Lovnish10@users.noreply.github.com>
2021-12-23 11:27:20 +0530

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -5,15 +5,15 @@ def call(script){
   // Utilities utl = new Utilities(script)
     
     node(){
-   
+    bat "echo 'Hello wolrd from bat'"
     echo 'hello world'
       echo "this value is given by :${script}"
       def maven = tool 'Maven 3.8.3'
       println maven.getClass()
       println "maven is ${maven}"
       
-      def records = [["key", "value"], ["a", "b"],["e","f"]]
-writeCSV file: 'output.csv', records: records, format: CSVFormat.EXCEL
+   //   def records = [["key", "value"], ["a", "b"],["e","f"]]
+//writeCSV file: 'output.csv', records: records, format: CSVFormat.EXCEL
         
     }
 }

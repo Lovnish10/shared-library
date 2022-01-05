2b13f8261a23d0d3a7083f4b9c096adb41ac478f
Lovnish10 <42857202+Lovnish10@users.noreply.github.com>
2022-01-05 20:27:19 +0530

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -6,21 +6,26 @@ def call(script){
     
     node(){
       
-      bat 'echo "Hello wolrd from bat ${BUILD_NUMBER}"'
-      bat "echo 'Hello wolrd from bat ${BUILD_NUMBER}'"
-    echo 'hello world'
-      echo "this value is given by :${script}"
-      def maven = tool 'Maven 3.8.3'
-      println maven.getClass()
-      println "maven is ${maven}"
+    //  bat 'echo "Hello wolrd from bat ${BUILD_NUMBER}"'
+   //   bat "echo 'Hello wolrd from bat ${BUILD_NUMBER}'"
+  //  echo 'hello world'
+   //   echo "this value is given by :${script}"
+   //   def maven = tool 'Maven 3.8.3'
+   //   println maven.getClass()
+   //   println "maven is ${maven}"
       
       //def records = [["k", "p"], ["r", "k"],["z","p"]]
       //writeCSV file: 'output.csv', records: records, format: CSVFormat.EXCEL
       
-      println "printing node version"
-      bat 'node -v'
-      println "printing npm latest version"
-      bat 'npm -v'
+    //  println "printing node version"
+    //  bat 'node -v'
+    //  println "printing npm latest version"
+    //  bat 'npm -v'
         
+      def pluginList = new ArrayList(Jenkins.instance.pluginManager.plugins)
+pluginList.sort { it.getShortName() }.each{
+  plugin -> 
+    println ("${plugin.getDisplayName()} (${plugin.getShortName()}): ${plugin.getVersion()}")
+}
     }
 }
